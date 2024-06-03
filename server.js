55bb9ecdf99647ec9061cc1ef1bba9838f34de04
Sheryl L <s_lawrence0@yahoo.ca>
2024-06-02 21:50:48 -0400

    Browser issue resolved, wildcard moved in server.js to bottom of code

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ app.use(express.static('public'));
 
 // GET route for notes page
 app.get('/notes', (req, res) => 
-  res.sendFile(path.join(__dirname, '/publc/notes.html'))
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/index.html'))
+// );
 
 // POST route for a new note
 // app.post('/notes', function (req, res) {
@@ -59,7 +59,7 @@ app.get('*', (req, res) =>
 
 // GET route to return all saved notes 
 app.get('/api/notes', (req, res) => {
-    fs.readFile('.//db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
         } else {
@@ -84,6 +84,10 @@ app.post('/api/notes', (req, res) => {
         });
     });
 });
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+  );
 
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT}`)
